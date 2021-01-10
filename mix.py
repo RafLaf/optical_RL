fb3ca9aaf91d431f614144360297567261f0b355
yan monier <yan.monier@ens-paris-saclay.fr>
2021-01-10 11:29:26 +0100

    indent correction

--- mix.py
+++ mix.py
@@ -10,57 +10,57 @@ from scipy.stats import multivariate_normal
 import cma
 
 class Net(nn.Module):
-    def __init__(self,rho):
-        super(Net, self).__init__()
-        self.pool3 = nn.MaxPool2d(2, 2)
-        self.conv1 = nn.Conv2d(3, 32, 5)              #convolution avec 3 channel entrée (RVB); 32 channel de sortie, kernel de 5*5 (pas sûr de moi pour le 5*5)
-        nn.init.normal_(self.conv1.weight)  #normal distribution
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 64, 5)
-        nn.init.normal_(self.conv2.weight)
-        self.conv3 = nn.Conv2d(64, 128, 5)
-        nn.init.normal_(self.conv3.weight)
-        #self.conv4 = nn.Conv2d(128, 256, 5)
-        #nn.init.normal_(self.conv4.weight)
-        self.Win=nn.Linear(512,512)
-        Nr,D=512,15
-        self.W=sc.random(Nr,Nr,density=float(D/Nr))
-        self.W=rho/max(abs(np.linalg.eigvals(self.W.A)))*self.W
-        self.W=(2*self.W-(self.W!=0))
-        self.W=torch.from_numpy(self.W.A)
-        self.r=torch.zeros(512)
-    def forward(self, x):
-        print(x.shape)
-        x=self.pool3(x)
-        print(x.shape)
-        x = self.pool(self.conv1(x))
-        print(x.shape)
-        x = self.pool(self.conv2(x))
-        print(x.shape)
-        x = self.pool(self.conv3(x))
-        print(x.shape)
-        #x = self.pool(self.conv4(x))
-        print(x.shape)
-        means = x.mean(dim=1, keepdim=True)
-        stds = x.std(dim=1, keepdim=True)
-        x = (x - means) / stds
-        s=x.size()
-        x=torch.reshape(x,(s[0],s[1]*s[2]*s[3]))
-        return x
-    def RCstep(self,x,aleak):
-        a=self.forward(x)
-        print('ok')
-        print(a.size(),self.r.size())
-        v1=torch.matmul(self.r,self.W.float())
-        v2=self.Win(a)
-        print(v1.size(),v2.size(),(v1+v2).size())
-        self.r=(1-aleak)*self.r+aleak*torch.tanh(v1+v2)
-        self.r=torch.reshape(self.r,(512,))
-        a=torch.reshape(a,(512,))
-        print(self.r.size(),a.size)
-        return torch.cat((self.r,a))
+	def __init__(self,rho):
+		super(Net, self).__init__()
+		self.pool3 = nn.MaxPool2d(2, 2)
+		self.conv1 = nn.Conv2d(3, 32, 5)			  #convolution avec 3 channel entrée (RVB); 32 channel de sortie, kernel de 5*5 (pas sûr de moi pour le 5*5)
+		nn.init.normal_(self.conv1.weight)	#normal distribution
+		self.pool = nn.MaxPool2d(2, 2)
+		self.conv2 = nn.Conv2d(32, 64, 5)
+		nn.init.normal_(self.conv2.weight)
+		self.conv3 = nn.Conv2d(64, 128, 5)
+		nn.init.normal_(self.conv3.weight)
+		#self.conv4 = nn.Conv2d(128, 256, 5)
+		#nn.init.normal_(self.conv4.weight)
+		self.Win=nn.Linear(512,512)
+		Nr,D=512,15
+		self.W=sc.random(Nr,Nr,density=float(D/Nr))
+		self.W=rho/max(abs(np.linalg.eigvals(self.W.A)))*self.W
+		self.W=(2*self.W-(self.W!=0))
+		self.W=torch.from_numpy(self.W.A)
+		self.r=torch.zeros(512)
+	def forward(self, x):
+		print(x.shape)
+		x=self.pool3(x)
+		print(x.shape)
+		x = self.pool(self.conv1(x))
+		print(x.shape)
+		x = self.pool(self.conv2(x))
+		print(x.shape)
+		x = self.pool(self.conv3(x))
+		print(x.shape)
+		#x = self.pool(self.conv4(x))
+		print(x.shape)
+		means = x.mean(dim=1, keepdim=True)
+		stds = x.std(dim=1, keepdim=True)
+		x = (x - means) / stds
+		s=x.size()
+		x=torch.reshape(x,(s[0],s[1]*s[2]*s[3]))
+		return x
+	def RCstep(self,x,aleak):
+		a=self.forward(x)
+		print('ok')
+		print(a.size(),self.r.size())
+		v1=torch.matmul(self.r,self.W.float())
+		v2=self.Win(a)
+		print(v1.size(),v2.size(),(v1+v2).size())
+		self.r=(1-aleak)*self.r+aleak*torch.tanh(v1+v2)
+		self.r=torch.reshape(self.r,(512,))
+		a=torch.reshape(a,(512,))
+		print(self.r.size(),a.size)
+		return torch.cat((self.r,a))
 
-    
+	
 net = Net(0.9)
 def launch_scenarios(Wout):
 	reward_list=[]
@@ -71,7 +71,7 @@ def launch_scenarios(Wout):
 		reward_sum=0
 		feature=torch.from_numpy(np.array(1024))
 		for t in range(100):
-			env.render()     #pour que ce soit visible à l'écran il suffit de décommenter cette ligne -> ralentit tout considerablement. *4 computing time
+			env.render()	 #pour que ce soit visible à l'écran il suffit de décommenter cette ligne -> ralentit tout considerablement. *4 computing time
 			print(i_episode,t,observation.shape)
 			
 			#generation d'action par WOUT et features
